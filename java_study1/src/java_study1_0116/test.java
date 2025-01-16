889bd1337cdef50dcafb32b4b32dac87d41f4e8d
KuntaePark <sgvelcro@naver.com>
2025-01-16 12:50:50 +0900

    20250116-1

--- java_study1/src/java_study1_0116/test.java
+++ java_study1/src/java_study1_0116/test.java
@@ -4,7 +4,18 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/* 
+		 * 주석 여러줄
+		 */
+		System.out.println("자바 이클립스 테스트");
+		
+		//java 데이터 타입:
+		//정수형 : short, int, long
+		//실수형 : float double
+		//문자 : char
+		//문자열 : String
+		int age;
+		age = 100;
 	}
 
 }
